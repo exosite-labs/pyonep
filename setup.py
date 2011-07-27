a8a70dd87ec242c097673896d6ef1ce412e7e0b4
hardev <labs@exosite.com>
2011-07-27 17:44:21 +0800

    updated setup package name

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from distutils.core import setup
 
-setup (name='pyonep',
+setup (name='onepv1lib',
        version='1.0',
        url = 'http://github.com/exosite-labs/pyonep',
        author = 'Exosite',
