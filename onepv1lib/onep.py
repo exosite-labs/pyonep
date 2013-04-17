72b3a8f3d3deb3b30384ba6cca6f172df2389a37
Dan Weaver <lastfinger@gmail.com>
2013-04-16 22:51:01 -0500

    Removed debug output

--- onepv1lib/onep.py
+++ onepv1lib/onep.py
@@ -52,7 +52,6 @@ class DeferredRequests():
 class OnepV1():
   headers = {'Content-Type': 'application/json; charset=utf-8'}
   def __init__(self,host='logicpd.m2.exosite.com',port='80',url='/api:v1/rpc/process',httptimeout=3,verbose=False):
-    print "OnepV1: " + host
     self.host        = host + ':' + port
     self.url         = url
     self.httptimeout = int(httptimeout)
