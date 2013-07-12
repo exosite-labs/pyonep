1e676e244a0ff1244f45e52dbeaff8ceed21e4e0
Dan Weaver <lastfinger@gmail.com>
2013-07-12 12:12:19 -0500

    0.5 https support

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='onepv1lib',
-      version='0.4',
-      url='http://github.com/exosite-labs/pyonep',
-      author='Exosite',
-      author_email='labs@exosite.com',
+      version='0.5',
+      url='http://github.com/dweaver/pyonep',
+      author='Dan Weaver',
+      author_email='danweaver@exosite.com',
       description='Python bindings for Exosite API over HTTP JSON RPC.',
       packages=['onepv1lib'],
       )
