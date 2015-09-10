763271fc613fb148c92b012f1bfefb2225bc450d
Dan Slimmon <dan.slimmon@gmail.com>
2015-09-10 16:04:32 +0000

    Added Markdown support for docs

--- docs/conf.py
+++ docs/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import shlex
+from recommonmark.parser import CommonMarkParser
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -34,10 +35,10 @@ extensions = []
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-# The suffix(es) of source filenames.
-# You can specify multiple suffix as a list of string:
-# source_suffix = ['.rst', '.md']
-source_suffix = '.rst'
+source_parsers = {
+        '.md': CommonMarkParser,
+}
+source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
