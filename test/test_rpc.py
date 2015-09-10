278be122815d063e9b810c124d5778ad0a7087ac
Dan Slimmon <dan.slimmon@gmail.com>
2015-09-10 22:39:26 +0000

    Run tests for all versions in parallel. Resolves #28.

--- test/test_rpc.py
+++ test/test_rpc.py
@@ -3,7 +3,14 @@
 '''Test pyonep RPC'''
 from __future__ import unicode_literals
 
-import test_base
+#@DEBUG
+import os
+import sys
+f=open("/tmp/whatever", "w")
+f.write("x-alpha: {0}\n".format(os.getcwd()))
+f.write("x-bravo: {0}\n".format(sys.path))
+f.close()
+from test import test_base
 
 
 class TestRPC(test_base.TestBase):
